4be0c9b45ea2206c0c86acb11c63b13e513cfae1
unknown <lara.sofie.franke@hs-furtwangen.de>
2020-11-05 11:49:14 +0100

    Update 05.11 1.1 Anfänge Typescript

---
+++ typescript/Aufgabe2.1/Uebung.ts
@@ -0,0 +1,8 @@
+//console.log(true + true);
+console.log(5 + 1);
+//console.log(5 + '1');
+//console.log('5' + 1);
+//console.log('5' - 1);
+
+//BONUS:
+//console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
