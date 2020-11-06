bb37530ac39eb01347df012b24c6d34701901c11
unknown <lara.sofie.franke@hs-furtwangen.de>
2020-11-06 09:59:48 +0100

    Update 06.11 1.2

---
+++ typescript/Aufgabe2.1/aufgabe2.js
@@ -0,0 +1,10 @@
+"use strict";
+function a2() {
+    let i = 9;
+    do {
+        console.log(i);
+        i = i - 1;
+    } while (i > 0);
+}
+a2();
+//# sourceMappingURL=aufgabe2.js.map
---
+++ typescript/Aufgabe2.1/aufgabe2.ts
@@ -0,0 +1,11 @@
+
+function a2(): void {
+    let i: number = 9;
+
+    do {
+        console.log(i);
+        i = i - 1;
+    } while( i > 0);
+}
+
+a2();
