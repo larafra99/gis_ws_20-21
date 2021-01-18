d3863247c733981c7c1deeb64971324fa280a1b9
larafra99 <lara.sofie.franke@hs-furtwangen.de>
2021-01-18 10:42:00 +0100

    Update 18.01 1.0 Anfänge 3.4

---
+++ Datenbanken/Aufgabe3.4/login.ts
@@ -0,0 +1,19 @@
+export namespace Aufgabe3_3 {
+    let form: HTMLFormElement = <HTMLFormElement>document.getElementById("form");
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
+    button.addEventListener("click", sendToServer);
+    let url: string = "https://gisws2021.herokuapp.com/";
+
+    async function sendToServer(_event: Event): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responseText: string = await response.text();
+        console.log(response);
+        console.log("Response Text: " + responseText);
+    }
+
+}
+
